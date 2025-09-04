5ad7ded955dbbdad3b72b018fa159e40e484692b
sanaa <Sanaakumar07@gmail.com>
2025-09-04 11:04:08 +0530

    Initial Commit

---
+++ stars/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  stars
+//
+//  Created by Sanaa Kumar on 04/09/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
